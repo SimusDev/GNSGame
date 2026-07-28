90bbb5397c8d7ca653062446cf164b1aab0b715c
SimusDev <simusdev@mail.ru>
2026-07-28 22:14:56 +0300

    sas

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace GNSGame
+﻿
+namespace GNSGame
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
