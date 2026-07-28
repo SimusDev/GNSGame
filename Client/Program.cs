2b1a6c8f4c337c5e84bed50ec1452c95232a5237
SimusDev <simusdev@mail.ru>
2026-07-29 02:46:04 +0300

    небольшой пуки каки

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,76 +1,26 @@
-﻿using Valve.Sockets;
-
+﻿
 namespace Client
 {
     internal class Program
     {
+        static Common.Network.MultiplayerAPI Multiplayer;
+        static Common.Core.Scene.World World;
+
         static void Main(string[] args)
         {
-            Valve.Sockets.Library.Initialize();
+            Multiplayer = new();
+            World = new();
 
-            NetworkingSockets client = new NetworkingSockets();
+            World.Initialize(Multiplayer);
 
-            uint connection = 0;
-
-            StatusCallback status = (ref StatusInfo info) => {
-                switch (info.connectionInfo.state)
-                {
-                    case ConnectionState.None:
-                        break;
-
-                    case ConnectionState.Connected:
-                        Console.WriteLine("Client connected to server - ID: " + connection);
-                        break;
-
-                    case ConnectionState.ClosedByPeer:
-                    case ConnectionState.ProblemDetectedLocally:
-                        client.CloseConnection(connection);
-                        Console.WriteLine("Client disconnected from server");
-                        break;
-                }
-            };
-
-
-            Address address = new Address();
-
-            connection = client.Connect(ref address);
-
-#if VALVESOCKETS_SPAN
-	MessageCallback message = (in NetworkingMessage netMessage) => {
-		Console.WriteLine("Message received from server - Channel ID: " + netMessage.channel + ", Data length: " + netMessage.length);
-	};
-#else
-            const int maxMessages = 20;
-
-            NetworkingMessage[] netMessages = new NetworkingMessage[maxMessages];
-#endif
-
-            while (!Console.KeyAvailable)
-            {
-                client.RunCallbacks();
-
-#if VALVESOCKETS_SPAN
-		client.ReceiveMessagesOnConnection(connection, message, 20);
-#else
-                int netMessagesCount = client.ReceiveMessagesOnConnection(connection, netMessages, maxMessages);
-
-                if (netMessagesCount > 0)
-                {
-                    for (int i = 0; i < netMessagesCount; i++)
-                    {
-                        ref NetworkingMessage netMessage = ref netMessages[i];
-
-                        Console.WriteLine("Message received from server - Channel ID: " + netMessage.channel + ", Data length: " + netMessage.length);
-
-                        netMessage.Destroy();
-                    }
-                }
-#endif
-
-                Thread.Sleep(15);
-            }
-
-            Valve.Sockets.Library.Deinitialize();
+            var loop = new Common.Core.Loop(Loop);
+            loop.Start();
         }
+
+        static void Loop(double delta)
+        {
+            World.Tick(delta);
+        }
+
     }
 }
