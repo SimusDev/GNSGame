481474ab21066eda6af8c0f243ce00d768775158
SimusDev <simusdev@mail.ru>
2026-07-29 14:14:56 +0300

    Create Logger.cs

---
+++ Common/Core/Debug/Logger.cs
@@ -0,0 +1,21 @@
+﻿namespace Common.Core.Debug
+{
+    public static class Logger
+    {
+        public static event Action<string>? PrintOutput;
+
+        public static void Print(params object[] args)
+        {
+            string result = "";
+            for (int i = 0; i < args.Length; i++)
+                result  += args[i].ToString();
+
+            Console.WriteLine(result);
+            PrintOutput?.Invoke(result);
+
+        }
+
+
+
+    }
+}
